f5b86db999ec014893d4b72be036cc4ceabb699a
Petr Messner <petr.messner@gmail.com>
2019-05-12 17:35:40 +0200

    frontend: add Github link to About page

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,9 +1,12 @@
 import Layout from '../components/Layout'
 
+const githubRepo = 'https://github.com/messa/graphql-workshop'
+
 function AboutPage() {
   return (
     <Layout currentPage='about'>
       <h1>About</h1>
+      <p>Github repo: <a href={githubRepo}>{githubRepo}</a></p>
     </Layout>
   )
 }
